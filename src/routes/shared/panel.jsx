35e7cdbb22db353d23169f00aa3ce91f3ec5019e
Jose Adalberto Lucas Lima <jalucas.jall@gmail.com>
2023-08-25 18:43:40 -0300

    Avanços na tela de clube (e painel compartilhado)
    Troca entre formulário e exibição está próxima da conclusão... [Ainda falta envio de form para o server]

--- src/routes/shared/panel.jsx
+++ src/routes/shared/panel.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react"
+import { Icon } from "@iconify/react"
+import "../../assets/panel.css"
+import { Form } from "react-router-dom"
+
 function PanelFigure({ url }) {
     return (
         <figure id="overview-figure" className="m-auto mb-2 overview-figure-clube position-relative">
@@ -7,7 +12,7 @@ function PanelFigure({ url }) {
                     ? `${url}`
                     : `${'/midia/imagens/kaffe.jpg'}`} />
             <div className="top-0 position-absolute w-100 h-100">
-                <label for="imageUpload">
+                <label htmlFor="imageUpload">
                     <i className="bi bi-pencil-square representante-edicao"></i>
                 </label>
                 <input className="d-none" type="file" accept=".png, .jpg, .jpeg" id="imageUpload" name="imageUpload" />
@@ -17,40 +22,96 @@ function PanelFigure({ url }) {
     )
 }
 
-function PanelInformations({ nome, subtitulo, descricao }) {
+function PanelInformations({ nome, subtitulo, descricao, isEditing }) {
+    const [showCompleteText, setShowCompleteText] = useState(false)
+
+    console.log(arguments)
+
     return (
-        <section id="overview-section" className="card-body w-100">
-            <h4 className="card-title can-edit fs-5">
-                {nome}
-            </h4>
-            <h5 className="card-subtitle mb-2 text-muted can-edit fs-6 small">
-                {subtitulo}
-            </h5>
-            <p className="card-text text-truncate descricao-truncate can-edit mb-1" aria-expanded="false">
-                {descricao}
-            </p>
-            <button id="clube-ler-mais-btn" className="d-block btn-link btn-ler-mais">« Ler mais »</button>
-            <section className="w-100 mt-2">
-            </section>
-        </section>
+        !isEditing
+            ? (
+                <section id="overview-section" className="card-body w-100" >
+                    {nome &&
+                        <h4 className="card-title can-edit fs-5">
+                            {nome}
+                        </h4>
+                    }
+                    {subtitulo &&
+                        <h5 className="card-subtitle mb-2 text-muted can-edit fs-6 small">
+                            {subtitulo}
+                        </h5>
+                    }
+                    {(descricao || bio) &&
+                        <p
+                            className={'card-text ' + `${showCompleteText ? 'text-truncate' : ''}` + ' descricao-truncate mb-1'}
+                            aria-expanded="false"
+                        >
+                            {descricao || bio}
+                        </p>
+                    }
+                    <button
+                        id="clube-ler-mais-btn"
+                        onClick={() => setShowCompleteText(!showCompleteText)}
+                        className="d-block btn-link btn-ler-mais"
+                    >
+                        « Ler mais »
+                    </button>
+                </section >
+            )
+            : (
+                <Form>
+                    {Object.keys(arguments[0]).slice(0, 3).map(item =>
+                        <div class="mb-3">
+                            <label
+                                htmlFor="exampleInputEmail1"
+                                class="form-label"
+                            >
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                            />
+                            <div
+                                id="emailHelp"
+                                class="form-text"
+                            >
+                                We'll never share your email with anyone else.
+                            </div>
+                        </div>
+                    )}
+                </Form>
+            )
     )
 }
 
 function Panel({ data }) {
-    console.log(data)
+    const [editState, setEditState] = useState(false)
+
     return (
         <div
             id="overview-container"
             className="card position-relative overview-pagina"
             style={{ marginRight: '20px', marginLeft: '10px' }}
         >
-            <PanelFigure url={data.url} />
+            <button
+                type="button"
+                onClick={() => setEditState(!editState)}
+                style={{ backgroundColor: "inherit" }}
+                className="panel-edicao position-absolute end-0 d-flex p-0 m-1 justify-content-center border rounded-circle"
+            >
+                <Icon icon="mdi:gear-outline" width={30} height={30} />
+            </button>
+            <PanelFigure url={data.url} isEditing={editState} />
             <PanelInformations
                 nome={data.nome}
                 subtitulo={data.subtitulo}
                 descricao={data.descricao}
+                isEditing={editState}
             />
-        </div>
+        </div >
     )
 }
 
